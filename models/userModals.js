8c3bd620ec6ca86e9c5570a6bf810fd429fe83eb
OHshajim <ajshajimmax@gmail.com>
2025-02-22 01:21:34 +0600

    user details update

--- models/userModals.js
+++ models/userModals.js
@@ -3,9 +3,15 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
-    email: { type: String, unique: true, required: true },
-    password: { type: String, required: true },
-    loans: [{ type: mongoose.Schema.Types.ObjectId, ref: "Loan" }],
+    email: { type: String, required: true, unique: true },
+    password: { type: String },
+    role: { type: String, enum: ["customer", "admin"], default: "customer" },
+    wishlist: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product" }],
+    cart: [
+      { productId: { type: mongoose.Schema.Types.ObjectId, ref: "Product" } },
+    ],
+    orders: [{ type: mongoose.Schema.Types.ObjectId, ref: "Order" }],
+    createdAt: { type: Date, default: Date.now },
   },
   { timestamps: true }
 );
