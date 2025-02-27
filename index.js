f26af7233bbb0ee0fcbbdb0182432c737fb0b8c2
OHshajim <ajshajimmax@gmail.com>
2025-02-27 17:58:05 +0600

    CORS UPDATE

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 require("dotenv").config();
-const { createClient } = require("@supabase/supabase-js");
 const cors = require("cors");
-const cloudinary = require("cloudinary").v2;
+const { createClient } = require("@supabase/supabase-js");
 const fileUpload = require("express-fileupload");
+const cloudinary = require("cloudinary").v2;
 cloudinary.config({
   cloud_name: process.env.CloudName,
   api_key: process.env.API_KEY,
@@ -14,12 +14,21 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors());
 app.use(
   fileUpload({
     useTempFiles: true,
   })
 );
+app.use(
+  cors({
+    origin: [
+      "https://teal-malasada-0a1afc.netlify.app/",
+      "http://localhost:5000",
+    ],
+    credentials: true,
+  })
+);
+
 // Connect to supabase
 const supabase = createClient(
   process.env.SUPABASE_URL,
