3a6f92eac58dc98e96e09c958b0608defbf1086d
OHshajim <ajshajimmax@gmail.com>
2025-02-27 17:24:54 +0600

    CORS UPDATE

--- index.js
+++ index.js
@@ -12,13 +12,20 @@ cloudinary.config({
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.use(cors());
+
 app.use(express.json());
 app.use(
   fileUpload({
     useTempFiles: true,
   })
 );
+app.use(
+  cors({
+    origin: ["https://teal-malasada-0a1afc.netlify.app"],
+    credentials: true,
+  })
+);
+
 // Connect to supabase
 const supabase = createClient(
   process.env.SUPABASE_URL,
